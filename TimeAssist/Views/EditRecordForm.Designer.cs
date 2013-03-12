dd1634d46c827eb93fe4fd793c3229e821e57c47
John Reffitt <znothegamer@gmail.com>
2013-03-12 13:05:58 -0400

    adding the new edit form.

---
+++ TimeAssist/Views/EditRecordForm.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace TimeAssist.Views
+{
+    partial class EditRecordForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dateTimePickerStart = new System.Windows.Forms.DateTimePicker();
+            this.labelStart = new System.Windows.Forms.Label();
+            this.labelFinish = new System.Windows.Forms.Label();
+            this.dateTimePickerFinish = new System.Windows.Forms.DateTimePicker();
+            this.labelTask = new System.Windows.Forms.Label();
+            this.textBoxTask = new System.Windows.Forms.TextBox();
+            this.buttonApply = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxComment = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // dateTimePickerStart
+            // 
+            this.dateTimePickerStart.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePickerStart.Location = new System.Drawing.Point(72, 3);
+            this.dateTimePickerStart.Name = "dateTimePickerStart";
+            this.dateTimePickerStart.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerStart.TabIndex = 0;
+            // 
+            // labelStart
+            // 
+            this.labelStart.AutoSize = true;
+            this.labelStart.Location = new System.Drawing.Point(12, 9);
+            this.labelStart.Name = "labelStart";
+            this.labelStart.Size = new System.Drawing.Size(32, 13);
+            this.labelStart.TabIndex = 1;
+            this.labelStart.Text = "Start:";
+            // 
+            // labelFinish
+            // 
+            this.labelFinish.AutoSize = true;
+            this.labelFinish.Location = new System.Drawing.Point(12, 35);
+            this.labelFinish.Name = "labelFinish";
+            this.labelFinish.Size = new System.Drawing.Size(37, 13);
+            this.labelFinish.TabIndex = 2;
+            this.labelFinish.Text = "Finish:";
+            // 
+            // dateTimePickerFinish
+            // 
+            this.dateTimePickerFinish.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimePickerFinish.Location = new System.Drawing.Point(72, 29);
+            this.dateTimePickerFinish.Name = "dateTimePickerFinish";
+            this.dateTimePickerFinish.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePickerFinish.TabIndex = 0;
+            // 
+            // labelTask
+            // 
+            this.labelTask.AutoSize = true;
+            this.labelTask.Location = new System.Drawing.Point(12, 60);
+            this.labelTask.Name = "labelTask";
+            this.labelTask.Size = new System.Drawing.Size(34, 13);
+            this.labelTask.TabIndex = 2;
+            this.labelTask.Text = "Task:";
+            // 
+            // textBoxTask
+            // 
+            this.textBoxTask.Location = new System.Drawing.Point(72, 56);
+            this.textBoxTask.Name = "textBoxTask";
+            this.textBoxTask.Size = new System.Drawing.Size(200, 20);
+            this.textBoxTask.TabIndex = 3;
+            // 
+            // buttonApply
+            // 
+            this.buttonApply.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.buttonApply.Location = new System.Drawing.Point(197, 227);
+            this.buttonApply.Name = "buttonApply";
+            this.buttonApply.Size = new System.Drawing.Size(75, 23);
+            this.buttonApply.TabIndex = 4;
+            this.buttonApply.Text = "Apply";
+            this.buttonApply.UseVisualStyleBackColor = true;
+            this.buttonApply.Click += new System.EventHandler(this.buttonApply_Click);
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Location = new System.Drawing.Point(116, 227);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 4;
+            this.buttonCancel.Text = "Cancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 84);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Comment";
+            // 
+            // textBoxComment
+            // 
+            this.textBoxComment.Location = new System.Drawing.Point(15, 101);
+            this.textBoxComment.Name = "textBoxComment";
+            this.textBoxComment.Size = new System.Drawing.Size(257, 120);
+            this.textBoxComment.TabIndex = 5;
+            this.textBoxComment.Text = "";
+            // 
+            // EditRecordForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.textBoxComment);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonApply);
+            this.Controls.Add(this.textBoxTask);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelTask);
+            this.Controls.Add(this.labelFinish);
+            this.Controls.Add(this.labelStart);
+            this.Controls.Add(this.dateTimePickerFinish);
+            this.Controls.Add(this.dateTimePickerStart);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "EditRecordForm";
+            this.Text = "EditRecordForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DateTimePicker dateTimePickerStart;
+        private System.Windows.Forms.Label labelStart;
+        private System.Windows.Forms.Label labelFinish;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFinish;
+        private System.Windows.Forms.Label labelTask;
+        private System.Windows.Forms.TextBox textBoxTask;
+        private System.Windows.Forms.Button buttonApply;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox textBoxComment;
+    }
+}
---
+++ TimeAssist/Views/EditRecordForm.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TimeAssist.Views
+{
+    public partial class EditRecordForm : Form
+    {
+        public Record Record { get { return record; } set { record = value; } }
+
+        public event EventHandler OnApplyClicked;
+
+        public EditRecordForm(Record r)
+        {
+            record = r;
+            InitializeComponent();
+            dateTimePickerStart.Value = record.Start;
+            dateTimePickerFinish.Value = record.Finish;
+            textBoxTask.Text = record.Task;
+            textBoxComment.Text = record.Comment;
+            Invalidate();
+        }
+
+        private void buttonApply_Click(object sender, EventArgs e)
+        {
+            record.Start = dateTimePickerStart.Value;
+            record.Finish = dateTimePickerFinish.Value;
+            record.Task = textBoxTask.Text;
+            record.Comment = textBoxComment.Text;
+
+            if (OnApplyClicked != null) OnApplyClicked(sender, e);
+
+            Close();
+        }
+
+        Record record;
+    }
+}
